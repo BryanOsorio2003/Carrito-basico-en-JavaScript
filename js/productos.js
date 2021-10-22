e0f940d28e241104bf1bf8735f771b2b586707cc
BryanOsorio2003 <bsosorio550@misena.edu.co>
2021-10-21 20:18:56 -0500

    proyecto carrito basico hecho en js, html, y css.

---
+++ js/productos.js
@@ -0,0 +1,6 @@
+// const productos = [
+//     {id:1,nombre: 'Armadura de diamante ',precio: 10},
+//     {id:2,nombre: 'Armadura de hierro ',precio: 2400000},
+//     {id:3,nombre: 'Armadura de netherite ',precio: 1200000},
+//     {id:4,nombre: 'Armadura de oro ',precio: 540000}
+// ]
